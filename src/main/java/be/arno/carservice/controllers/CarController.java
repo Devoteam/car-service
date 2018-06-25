c3140fc81e3aa7bc17b35d2412bd1389d9526726
Arno Bruynseels <arno.bruynseels@telenet.be>
2018-06-25 11:14:18 +0200

    Added feign to fetch cars for companies

--- src/main/java/be/arno/carservice/controllers/CarController.java
+++ src/main/java/be/arno/carservice/controllers/CarController.java
@@ -1,8 +1,11 @@
 package be.arno.carservice.controllers;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,22 @@ public class CarController {
   public String helloFromCarService() {
     return "Hello from Car-Service";
   }
+
+  @GetMapping(value = "/companies/{companyId}")
+  public List<String> getAllCarsForCompany(@PathVariable int companyId) {
+
+    switch (companyId) {
+      case 1:
+        return newArrayList("BWM1", "AUDI A3", "MERCEDES A");
+      case 2:
+        return newArrayList("BWM2", "AUDI A4", "MERCEDES B");
+      case 3:
+        return newArrayList("BWM3", "AUDI A5", "MERCEDES C");
+      default:
+        return newArrayList("BWM4", "AUDI A6", "MERCEDES S");
+    }
+
+  }
+
 
 }
